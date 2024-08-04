bb41c9fe0a4c76dd5b7d07681b404d706ffe077e
lucca23 <luccamanfredotti@gmail.com>
2024-08-04 19:07:30 +0200

    Avanzamos en el index

---
+++ index.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="style.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Caveat&display=swap" rel="stylesheet">
+</head>
+<body>
+    <?php include "header y footer/header.html"; ?>
+    <div class="background-image">
+        <h1>LMG</h1>
+        <p>Lucca Manfredotti García</p>
+    </div>
+    <div class="divCurriculumFoto">
+    <div class="divCurriculumFotoIndex">
+    <div class="textContainerIndex">
+        <p class="textContentIndex">Estudiante de Ingeniería del Software con un Grado Superior en Desarrollo de Aplicaciones Web (DAW). Cuento con una sólida base técnica y experiencia en programación y diseño de bases de datos. Busco oportunidades para aplicar mis conocimientos y seguir creciendo profesionalmente en el desarrollo de software, contribuyendo a proyectos innovadores y trabajando en equipo.</p>
+        <a href="#curriculum" class="btnIndex">Ver Curriculum</a>
+    </div>
+    <img src="img/luccaFoto.png" alt="Imagen relacionada" class="imageIndex">
+</div>
+
+</body>
+</html>
